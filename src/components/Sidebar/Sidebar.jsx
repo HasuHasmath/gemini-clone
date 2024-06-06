e4fa58d188bed4da8102e82e8c06ed2cc158d3f7
hasuhasmath <hasuh5pepe@gmail.com>
2024-06-06 14:06:41 +0530

    Initial commit

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import './Sidebar.css'
+import {assets} from '../../assets/assets'
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="top">
+        <img className='menu' src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+            <img src="" alt="" />
+        </div>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  )
+}
+
+export default Sidebar
